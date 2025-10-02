70213d5477345ef943a29e37053353fee0d6f2b3
Morozov Mikhail <m.a.m2003@mail.ru>
2025-10-02 23:20:50 +0300

    Create errorSlice

---
+++ frontend/src/redux/slices/errorSlice.js
@@ -0,0 +1,26 @@
+const { createSlice } = require("@reduxjs/toolkit")
+
+const initialState = ''
+
+const errorSlice = createSlice({
+    name: 'error',
+    initialState,
+    reducers: {
+        setError: (state, action) => {
+            state = action.payload
+            return state
+        },
+        clearError: () => {
+            return initialState
+        }
+    }
+}
+)
+
+const { setError, clearError } = errorSlice.actions
+
+export function selectError(state) {
+    return state.error
+}
+
+export default errorSlice.reducer
